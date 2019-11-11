f167c4c3d1d63fe9fb8e004fdbaf3f9cba029947
Harsh-Pandey <harshp@smartjoules.in>
2019-11-11 16:18:22 +0530

    tested encryption

--- security/security.py
+++ security/security.py
@@ -1,12 +1,19 @@
-from speck import SpeckCipher
-
+#from speck import SpeckCipher
+from Crypto.Cipher import AES
 class Security:
 
-    def __init__(self,bit,mode=None):
+    def __init__(self,bit,mode):
         self.bit = bit
-        self.key = "something"
+        self.key = bytes('abcdefghijklmnop',encoding='utf-8')
         self.mode = mode
+        print(self.mode)
     
+    def padding(self,originalPayload):
+        
+        paddedPayload = originalPayload+"\0" *(AES.block_size-len(originalPayload)%AES.block_size)
+        print(paddedPayload)
+        return paddedPayload
+
     def speckEncryptor(self):
         pass
 
@@ -14,22 +21,28 @@ class Security:
         pass
 
     def aesEncryptor(self,payload):
-        from Crypto.Cipher import AES
-        if not self.mode:
-            self.mode = AES.MODE_EAX
-        cipher = AES.new(self.key, self.mode)
-        nonce = cipher.nonce
-        encryptedPayload, tag = cipher.encrypt_and_digest(payload)
-        return encryptedPayload
+        try:
+            print("Called!!")
+            from Crypto.Cipher import AES
+            if not self.mode:
+                self.mode = AES.MODE_ECB
+            cipher = AES.new(self.key, AES.MODE_ECB)
+            payload = self.padding(payload)
+            encryptedPayload = cipher.encrypt(payload)
+            print("Encrypted Data>>",encryptedPayload)
+            return encryptedPayload
+        except Exception as e:
+            print(e)
 
     def aesDecryptor(self,encryptedPayload):
+    
         from Crypto.Cipher import AES
         if not self.mode:
-            self.mode = AES.MODE_EAX
-        cipher = AES.new(self.key, self.mode, nonce=nonce)
+            self.mode = AES.MODE_ECB
+        cipher = AES.new(self.key,AES.MODE_ECB)
         originalPayload = cipher.decrypt(encryptedPayload)
         try:
-            cipher.verify(tag)
+            #cipher.verify(tag)
             print("The message is authentic:", originalPayload)
             return originalPayload
         except ValueError:
